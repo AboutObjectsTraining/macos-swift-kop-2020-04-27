681ca326150210ab84e40b3a71c94d0701b51327
Jonathan Lehr <jonathan@aboutobjects.com>
2020-04-29 13:59:29 -0400

    Initial version of Coolness

---
+++ Coolness/Coolness/AppDelegate.swift
@@ -0,0 +1,39 @@
+// Copyright (C) 2020 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+import Cocoa
+
+private let title = "My Window"
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+    
+    var windowDelegate = WindowDelegate()
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        print("In \(#function)")
+        
+        let rect = NSRect(x: 400, y: 400, width: 400, height: 400)
+        
+        window = NSWindow(contentRect: rect,
+                          styleMask: [.titled, .resizable, .miniaturizable],
+                          backing: .buffered,
+                          defer: true)
+        
+        window.delegate = windowDelegate
+        
+        defer {
+            window.orderFront(nil)
+        }
+        
+        guard let frame = NSScreen.main?.frame else { return }
+        window.setFrameOrigin(NSPoint(x: frame.midX, y: frame.midY))
+
+        window.title = title
+        window.setFrameAutosaveName(title)
+        window.isReleasedWhenClosed = false
+    }
+}
+
---
+++ Coolness/Coolness/WindowDelegate.swift
@@ -0,0 +1,12 @@
+// Copyright (C) 2020 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+import Cocoa
+
+class WindowDelegate: NSObject, NSWindowDelegate
+{
+//    func windowWillResize(_ sender: NSWindow, to frameSize: NSSize) -> NSSize {
+//        print("New size: \(frameSize)")
+//        return frameSize
+//    }
+}
