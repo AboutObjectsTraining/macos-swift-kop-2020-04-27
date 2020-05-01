31693e5fbdb3edf7d69f80070ab4323d7c020ac4
Jonathan Lehr <jonathan@aboutobjects.com>
2020-05-01 13:09:43 -0400

    Friday lunchtime WIP

--- Coolness2/Coolness2/AppDelegate.swift
+++ Coolness2/Coolness2/AppDelegate.swift
@@ -3,83 +3,26 @@
 
 import Cocoa
 
-private let title = "My Window"
-
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet var window: NSWindow!
     @IBOutlet var coolController: CoolController!
     
-    var inspectorPanel: NSPanel!
+    @IBOutlet var inspectorPanel: NSPanel!
+    @IBOutlet var inspectorPanelController: InspectorPanelController!
     
-//    var windowDelegate = WindowDelegate()
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        print("In \(#function)")
-        
+        inspectorPanel.contentViewController = inspectorPanelController
         window.contentViewController = coolController
+        window.isExcludedFromWindowsMenu = true
         window.makeKey()
-        
-//        configureMainWindow()
-        configureInspectorPanel()
-        configureMenuItems()
     }
     
-    @objc func showMainWindow(sender: Any?) {
-        window.orderFront(sender)
+    @IBAction func showMainWindow(_ sender: Any?) {
+        window.makeKeyAndOrderFront(sender)
     }
-    @objc func showInspectorPanel(sender: Any?) {
+    @IBAction func showInspectorPanel(_ sender: Any?) {
         inspectorPanel.orderFront(sender)
     }
-    
-    private func configureMenuItems() {
-        guard let windowMenu = NSApp.windowsMenu else { return }
-        windowMenu.addItem(withTitle: "My Window",
-                           action: #selector(showMainWindow(sender:)),
-                           keyEquivalent: "1")
-        windowMenu.addItem(withTitle: "Inspector",
-                           action: #selector(showInspectorPanel(sender:)),
-                           keyEquivalent: "2")
-        
-        window.isExcludedFromWindowsMenu = true
-    }
-    
-//    private func configureMainWindow() {
-//        let rect = NSRect(x: 400, y: 400, width: 400, height: 400)
-//
-//        window = NSWindow(contentRect: rect,
-//                          styleMask: [.titled, .resizable, .miniaturizable, .closable],
-//                          backing: .buffered,
-//                          defer: true)
-//
-//        window.delegate = windowDelegate
-//
-//        defer {
-//            window.orderFront(nil)
-//            window.makeKey()
-//        }
-//
-//        guard let frame = NSScreen.main?.frame else { return }
-//        window.setFrameOrigin(NSPoint(x: frame.midX, y: frame.midY))
-//
-//        window.title = title
-//        window.setFrameAutosaveName(title)
-//        window.isReleasedWhenClosed = false
-//        window.contentViewController = CoolController()
-//    }
-    
-    private func configureInspectorPanel() {
-        let panelFrame = NSRect(x: 700, y: 700, width: 240, height: 200)
-        inspectorPanel = NSPanel(contentRect: panelFrame,
-                                 styleMask: [.closable, .titled],
-                                 backing: .buffered,
-                                 defer: true)
-        inspectorPanel.contentViewController = InspectorPanelController()
-        inspectorPanel.setFrameAutosaveName("Inspector Panel")
-        inspectorPanel.title = "Inspector"
-        
-        inspectorPanel.orderFront(self)
-    }
 }
-
--- Coolness2/Coolness2/InspectorPanelController.swift
+++ Coolness2/Coolness2/InspectorPanelController.swift
@@ -5,22 +5,24 @@ import Cocoa
 
 class InspectorPanelController: NSViewController
 {
-    var textField: NSTextField!
+    @IBOutlet var textField: NSTextField!
     
-    override func loadView() {
-        let box = NSBox()
-        box.titlePosition = .noTitle
+    func loadViewXXX() {
+//        let box = NSBox()
+//        box.titlePosition = .noTitle
+//
+//        view = box
+//        view.setFrameSize(NSSize(width: 200, height: 160))
+//
+//        textField = NSTextField(frame: NSRect(x: 15, y: 90, width: 160, height: 30))
+//        textField.placeholderString = "Enter a label"
+//        view.addSubview(textField)
         
-        view = box
-        view.setFrameSize(NSSize(width: 200, height: 160))
-        
-        textField = NSTextField(frame: NSRect(x: 15, y: 90, width: 160, height: 30))
-        textField.placeholderString = "Enter a label"
-        view.addSubview(textField)
-        
-        let addButton = NSButton(title: "Add Cell",
-                                 target: nil,
-                                 action: #selector(CoolController.addCell(_:)))
-        view.addSubview(addButton)
+        // TODO: Wire up in IB
+        //
+        let _ = NSButton(title: "Add Cell",
+                         target: nil,
+                         action: #selector(CoolController.addCell(_:)))
+//        view.addSubview(addButton)
     }
 }
